5e49d84a7cfa689f9729579375bb280d49d18d33
Pablo Rodríguez <paproninja@gmail.com>
2026-02-15 21:48:43 +0100

    Version 1.12 Bruteforcing rotation cipher says what key it used for each iteration.

--- crypto.py
+++ crypto.py
@@ -230,7 +230,7 @@ def rot(text, key, operation):
 
     if operation == "bruteforce":
         for i in range(26):     # Brute forces the text with all possible keys
-            print(rot_logic(text, i))
+            print("Rot " + str(i) + ": " + rot_logic(text, i))
         return None
 
     if operation == "generate":
